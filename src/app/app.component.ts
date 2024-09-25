c8b2d0c257d853720a62603112b467a0dfa9ae61
Łukasz <wilk9911@gmail.com>
2024-09-26 00:31:49 +0200

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { PeriodicTableComponent } from './periodic-table/periodic-table.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: true,
+  imports: [PeriodicTableComponent],
+
 })
-export class AppComponent {
-  title = 'atiperaTask';
-}
+export class AppComponent {}
